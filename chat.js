087e78c3b5180a271b01795a6b8ccbad0c04f0fb
Tabatha <tat3534@thi.de>
2024-11-25 18:58:06 +0100

    Ein Schritt zurück

--- chat.js
+++ chat.js
@@ -3,7 +3,6 @@
 const backendUrl = "https://online-lectures-cs.thi.de/chat/ba1ad2f8-7e88-4ce4-92c2-6399ab16f647";
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNzMyMzkwOTQwfQ.DQA6mSt-oo4qPZ0N09zS2W6Cd_2g4BJpn4qL_zr24dw";
 
-
 // Chat Partner aus der URL holen
 function getChatPartner() {
     const url = new URL(window.location.href); 
@@ -11,29 +10,15 @@ function getChatPartner() {
     return queryParams.get("friend") || "Unknown";
 }
 
-let firstLoad = true;
-
 // Nachrichten aktualisieren
 function updateMessages(messages) {
     const messageList = document.querySelector('.message-list');
-    if (firstLoad) {
-        messageList.innerHTML = "";
-        messages.forEach(msg => {
-            const li = document.createElement('li');
-            li.textContent = `${msg.from}: ${msg.msg}`;
-            messageList.appendChild(li);
-        });
-    } else {
-        const currentMessages = [...messageList.children].map(li => li.textContent);
-        messages.forEach(msg => {
-            const newMessage = `${msg.from}: ${msg.msg}`;
-            if (!currentMessages.includes(newMessage)) { 
-                const li = document.createElement('li');
-                li.textContent = newMessage;
-                messageList.appendChild(li);
-            }
-        });
-    }
+    messageList.innerHTML = "";
+    messages.forEach(msg => {
+        const li = document.createElement('li');
+        li.textContent = `${msg.from}: ${msg.msg}`;
+        messageList.appendChild(li);
+    });
 }
 
 // Nachrichten laden
@@ -43,12 +28,7 @@ function loadMessages() {
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
             const messages = JSON.parse(xmlhttp.responseText);
-            if (firstLoad) {
-                updateMessages(messages.slice(0, 2));
-                firstLoad = false; 
-            } else {
-                updateMessages(messages);
-            }
+            updateMessages(messages);
         }
     };
     xmlhttp.open("GET", `${backendUrl}/message/${chatPartner}`, true);
