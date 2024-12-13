955ef552b1c91a0749e6cdfe11249b780c5746d5
Tabatha <tat3534@thi.de>
2024-12-13 05:37:44 +0100

    start.php wieder mit Fehlerbericht...

--- start.php
+++ start.php
@@ -1,13 +1,21 @@
 <?php
-session_start();
+// Fehlerberichterstattung aktivieren
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-spl_autoload_register(function ($class) {
-    include str_replace('\\', '/', $class) . '.php';
+// Autoload für Klassen
+spl_autoload_register(function ($class_name) {
+    include str_replace('\\', '/', $class_name) . '.php';
 });
 
-// Die Collection-ID aus Ihrer Datei:
+// Session starten
+session_start();
+
+// Backend-Konfiguration
 define('CHAT_SERVER_URL', 'https://online-lectures-cs.thi.de/chat/');
 define('CHAT_COLLECTION_ID', 'ba1ad2f8-7e88-4ce4-92c2-6399ab16f647');
 
+// Initialisieren des Backend-Dienstes
 $service = new Utils\BackendService(CHAT_SERVER_URL, CHAT_COLLECTION_ID);
 ?>
